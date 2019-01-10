0edd8a01b21efc7a2716a3fbcd84a82aa1318061
Chisty <chistyinfo@gmail.com>
2019-01-10 07:19:44 +0600

    lesson-17

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -6,17 +6,15 @@ class ProductDetails extends StatefulWidget {
   final product_details_oldprice;
   final product_picture;
 
-  ProductDetails({
-    this.product_details_name,
-    this.product_details_newprice,
-    this.product_details_oldprice,
-    this.product_picture
-    });
+  ProductDetails(
+      {this.product_details_name,
+      this.product_details_newprice,
+      this.product_details_oldprice,
+      this.product_picture});
   _ProductDetailsState createState() => _ProductDetailsState();
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
-   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,12 +37,112 @@ class _ProductDetailsState extends State<ProductDetails> {
           Container(
             height: 300.0,
             child: GridTile(
-    
               child: Container(
                 color: Colors.white,
                 child: Image.asset(widget.product_picture),
               ),
+              footer: Container(
+                color: Colors.white70,
+                child: ListTile(
+                  leading: Text(
+                    widget.product_details_name,
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                  ),
+                  title: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          "\$${widget.product_details_oldprice}",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              decoration: TextDecoration.lineThrough),
+                        ),
+                      ),
+                      Expanded(
+                        child: Text(
+                          "\$${widget.product_details_newprice}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.red),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
+          ),
+          //===================== 1st Button Row =======================
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 2.0,
+                  child: Row(
+                    children: <Widget>[
+                      //========== Size button ===============
+                      Expanded(
+                        child: Text("Size"),
+                      ),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 2.0,
+                  child: Row(
+                    children: <Widget>[
+                      //========== Colors button ===============
+                      Expanded(
+                        child: Text("Colors"),
+                      ),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.white,
+                  textColor: Colors.grey,
+                  elevation: 2.0,
+                  child: Row(
+                    children: <Widget>[
+                      //========== Qty button ===============
+                      Expanded(
+                        child: Text("Qty"),
+                      ),
+                      Expanded(child: Icon(Icons.arrow_drop_down)),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+          //===================== Buy Button Row =======================
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: MaterialButton(
+                  onPressed: () {},
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  elevation: 2.0,
+                  child: Text("Buy"),
+                ),
+              ),
+              IconButton(icon: Icon(Icons.add_shopping_cart,color: Colors.red,),onPressed: (){},),
+              IconButton(icon: Icon(Icons.favorite,color: Colors.red,),onPressed: (){},)
+            ],
           )
         ],
       ),
